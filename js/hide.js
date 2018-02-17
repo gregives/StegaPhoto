6712e7be4784cc1ad64b39efe61ebb10a3b54d06
Greg Ives <gregives774@gmail.com>
2018-02-17 01:21:08 +0000

    Create fileHandler module and web worker to hide files

---
+++ js/hide.js
@@ -0,0 +1,94 @@
+this.onmessage = function (e) {
+  const worker = this
+
+  const files = e.data.files
+  const image = e.data.image
+  const password = e.data.password
+
+  // Import scripts.
+  worker.importScripts('../3rdp/jszip.min.js')
+
+  // Read image file as array buffer.
+  const reader = new this.FileReader()
+  reader.readAsArrayBuffer(image)
+  reader.onload = function () {
+    worker.postMessage({
+      status: 0
+    })
+
+    // Array buffer to Uint8Array.
+    const imageUint = new Uint8Array(reader.result)
+
+    // Create new zip and add files.
+    const zip = new worker.JSZip()
+    for (let i = 0; i < files.length; i++) {
+      zip.file(files[i].name, files[i])
+    }
+
+    // Generate zip file as a Uint8Array.
+    zip.generateAsync({
+      type: 'uint8array',
+      compression: 'DEFLATE',
+      compressionOptions: { level: 9 }
+    }).then(function (zipUint) {
+      worker.postMessage({
+        status: 1
+      })
+
+      const passwordUint = new worker.TextEncoder().encode(password)
+
+      // Import key using password.
+      worker.crypto.subtle.importKey('raw', passwordUint, {
+        name: 'PBKDF2'
+      }, false, ['deriveKey']).then(function (derKey) {
+        // Derivation algorithm.
+        const derAlg = {
+          name: 'PBKDF2',
+          salt: worker.crypto.getRandomValues(new Uint8Array(16)),
+          iterations: 1000,
+          hash: { name: 'SHA-256' }
+        }
+
+        // Derive new key from PBKDF2 key.
+        worker.crypto.subtle.deriveKey(derAlg, derKey, {
+          name: 'AES-CTR',
+          length: 256
+        }, false, ['encrypt']).then(function (encKey) {
+          worker.postMessage({
+            status: 2
+          })
+
+          // Encryption algorithm.
+          const encAlg = {
+            name: 'AES-CTR',
+            counter: new Uint8Array(16),
+            length: 128
+          }
+
+          // Encrypt data using new key.
+          worker.crypto.subtle.encrypt(encAlg, encKey, zipUint.buffer).then(function (encrypted) {
+            const zipUint = new Uint8Array(encrypted)
+
+            // Concatenate image and encrypted zip.
+            const resultUint = new Uint8Array(imageUint.length + zipUint.length)
+            resultUint.set(imageUint, 0)
+            resultUint.set(zipUint, imageUint.length)
+
+            // Get type of image.
+            const type = image.type.slice(6)
+
+            // Convert result to blob.
+            const resultBlob = new worker.Blob([resultUint], { type: `image/${type}` })
+
+            worker.postMessage({
+              status: 3,
+              result: resultBlob
+            })
+
+            worker.close()
+          })
+        })
+      })
+    })
+  }
+}
