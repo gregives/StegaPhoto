2d5e8bc0b9dac830f7b3e11b44e53d8f5937e5c3
Greg Ives <gregives774@gmail.com>
2020-10-01 21:28:12 +0100

    Remove Inter semibold

--- components/page.js
+++ components/page.js
@@ -7,10 +7,10 @@ const Page = (props) =>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>StegaPhoto</title>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" />
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" media="print" onLoad="this.media='all'" />
+            <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter&display=swap" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter&display=swap" media="print" onLoad="this.media='all'" />
             <noscript>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter&display=swap" />
             </noscript>
         </Head>
         {props.children}
