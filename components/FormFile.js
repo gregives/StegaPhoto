0ead15fbfab19dad4c34733185dfd789d4902536
Greg Ives <gregives774@gmail.com>
2020-10-06 00:04:32 +0100

    Improve form file component

--- components/FormFile.js
+++ components/FormFile.js
@@ -4,14 +4,26 @@ const FormFile = (props) => {
     const [files, setFiles] = useState([]);
     const fileInput = useRef(null);
 
+    const uniqueFiles = (uniqueFiles, file) => {
+        const exists = uniqueFiles.find(
+            (existing) =>
+                existing.name === file.name &&
+                existing.lastModified === file.lastModified &&
+                existing.size === file.size &&
+                existing.type === file.type
+        );
+
+        return exists ? uniqueFiles : [...uniqueFiles, file];
+    };
+
     return (
-        <ul
-            for={props.id}
-            className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-gray-900 bg-opacity-25 shadow-inner h-64 py-8 overflow-y-auto rounded-lg"
-        >
+        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-8 bg-gray-900 bg-opacity-25 shadow-inner py-8 overflow-y-auto -mx-8 sm:-mx-4 sm:rounded-lg">
             {files.map((file, index) => (
-                <li className="flex flex-col place-items-center text-center text-sm break-words p-2 mb-8">
-                    <span className="relative p-8 mb-4 w-1/3 bg-blue-700 bg-opacity-25 overflow-hidden rounded not-italic">
+                <li
+                    key={index}
+                    className="flex flex-col place-items-center text-center text-sm p-2"
+                >
+                    <span className="relative py-8 px-10 mb-4 w-1/3 bg-blue-700 bg-opacity-25 overflow-hidden rounded not-italic">
                         <button
                             className="absolute top-0 right-0 px-2 -mt-2 text-3xl rounded-bl-lg focus:outline-none focus-visible:shadow-outline hover:bg-blue-500 hover:bg-opacity-25 text-gray-500 hover:text-gray-100"
                             onClick={() =>
@@ -23,7 +35,7 @@ const FormFile = (props) => {
                             &times;
                         </button>
                     </span>
-                    {file.name}
+                    <span className="w-full truncate">{file.name}</span>
                 </li>
             ))}
             <li>
@@ -32,25 +44,32 @@ const FormFile = (props) => {
                     id={props.id}
                     name={props.name}
                     onChange={(event) =>
-                        setFiles([...files, ...event.target.files])
+                        setFiles(
+                            [...files, ...event.target.files].reduce(
+                                uniqueFiles,
+                                []
+                            )
+                        )
                     }
                     multiple={props.multiple}
                     hidden
                     ref={fileInput}
                 />
                 <label
-                    for={props.id}
-                    className="flex flex-col place-items-center text-center text-sm break-words p-2 mb-8"
+                    htmlFor={props.id}
+                    className="flex flex-col place-items-center text-center text-sm p-2"
                 >
                     <button
-                        className="relative p-8 mb-4 w-1/3 focus:outline-none focus-visible:shadow-outline bg-blue-700 hover:bg-blue-500 bg-opacity-25 hover:bg-opacity-25 rounded cursor-pointer"
+                        className="relative py-8 px-10 mb-4 w-1/3 focus:outline-none focus-visible:shadow-outline bg-blue-700 hover:bg-blue-500 bg-opacity-25 hover:bg-opacity-25 rounded cursor-pointer"
                         onClick={() => fileInput.current.click()}
                     >
                         <span className="absolute inset-0 h-full w-full text-4xl text-gray-500 hover:text-gray-100 focus:text-gray-100">
                             +
                         </span>
                     </button>
-                    {props.multiple ? "Add files" : "Add file"}
+                    <span className="w-full truncate">
+                        {props.multiple ? "Add files" : "Add file"}
+                    </span>
                 </label>
             </li>
         </ul>
