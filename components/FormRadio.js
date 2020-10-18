8b268e53284c1be26cdca1cfd143bce227646b37
Greg Ives <gregives774@gmail.com>
2020-10-18 16:51:43 +0100

    Revert "Change style of form radio component"
    This reverts commit 6de331758fad3ced033bd50f76d045f3084189a4.

--- components/FormRadio.js
+++ components/FormRadio.js
@@ -1,17 +1,12 @@
 const FormRadio = (props) => (
     <>
-        <div className="relative">
-            <div className="absolute w-full h-full border-2 border-transparent pointer-events-none">
-                <div className="w-full h-full border-2 border-gray-800 rounded-full"></div>
-            </div>
-            <input
-                type="radio"
-                id={props.id}
-                name={props.name}
-                className="block appearance-none focus:outline-none focus-visible:shadow-outline rounded-full border-2 border-current p-3 checked:bg-orange-500"
-                onChange={(event) => props.onChange(event)}
-            />
-        </div>
+        <input
+            type="radio"
+            id={props.id}
+            name={props.name}
+            className="appearance-none focus:outline-none focus-visible:shadow-outline rounded-full border-2 border-current p-3 checked:bg-orange-500"
+            onChange={(event) => props.onChange(event)}
+        />
         <label htmlFor={props.id} className="pl-4">
             {props.label}
         </label>
