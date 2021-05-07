792bb6bc4a14d3a334d55d4280e2d20261de72f9
Greg Ives <gregives774@gmail.com>
2021-05-07 09:59:39 +0100

    Use preact/compat in production

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-    webpack(config, { defaultLoaders }) {
+    webpack(config, { defaultLoaders, dev, isServer }) {
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+                react: "preact/compat",
+                "react-dom": "preact/compat",
+            });
+        }
+
         config.module.rules.push({
             test: /\.worker.js$/,
             use: ["worker-loader", defaultLoaders.babel],
